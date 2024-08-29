5b1856b37d36deb3e8143273067c4ff96d9ed44a
bilal <157973812+hmbilal108@users.noreply.github.com>
2024-08-29 23:11:34 +0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -15,5 +15,5 @@ def client(app):
 def test_home(client):
     response = client.get('/')
     assert response.status_code == 200
-    expected_text = 'Well done! You Successfully deployed your flask app, Now you can start building your app.'
+    expected_text = 'Well done! You Successfully deployed your flask app, Now you can start building your app. Assalam-o-Alaikum Abdullah Bhai'
     assert expected_text.encode() in response.data
